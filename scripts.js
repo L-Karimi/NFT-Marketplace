492e2f3c017272e1babe4934a658a2df26d4cb16
L-Karimi <luciahkei101@gmail.com>
2022-07-28 09:04:31 +0300

    nav bar variables

--- scripts.js
+++ scripts.js
@@ -1,1 +1,6 @@
 const elemToggleFunc = function (elem) { elem.classList.toggle("active"); }
+
+const navbar = document.querySelector("[data-navbar]");
+const navbarToggleBtn = document.querySelector("[data-navbar-toggle-btn]");
+
+navbarToggleBtn.addEventListener("click", function () { elemToggleFunc(navbar); });
