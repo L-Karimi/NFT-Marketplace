b3997c0f457277022e8fff9e418a39f0c7556ca8
L-Karimi <luciahkei101@gmail.com>
2022-07-28 09:05:33 +0300

    added an event lister on the wishlist buttons

--- scripts.js
+++ scripts.js
@@ -4,3 +4,10 @@ const navbar = document.querySelector("[data-navbar]");
 const navbarToggleBtn = document.querySelector("[data-navbar-toggle-btn]");
 
 navbarToggleBtn.addEventListener("click", function () { elemToggleFunc(navbar); });
+const whishlistBtn = document.querySelectorAll("[data-whishlist-btn]");
+
+for (let i = 0; i < whishlistBtn.length; i++) {
+
+  whishlistBtn[i].addEventListener("click", function () { elemToggleFunc(this); });
+
+}
